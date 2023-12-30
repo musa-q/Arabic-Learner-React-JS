4091987bcf413ba824b0d49a6d72c38743a1be3b
Musa Qureshi <musaqureshi2002@gmail.com>
2023-12-30 17:18:07 +0000

    Added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/HomePage'
+import VerbsPage from './pages/VerbsPage'
+import WordsPage from './pages/WordsPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MyNavBar from './components/NavBar';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('home');
+
+  const navigateToPage = (page) => {
+    setCurrentPage(page);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="dark-background" data-bs-theme="dark">
+      <MyNavBar onNavigate={navigateToPage} />
+      {currentPage === 'home' && <HomePage onNavigate={navigateToPage} />}
+      {currentPage === 'verbs' && <VerbsPage />}
+      {currentPage === 'words' && <WordsPage />}
+    </div>
   )
 }
 
