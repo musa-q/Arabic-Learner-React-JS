4bd569be60e19d01eeeff9e8f579942ac9212b5f
Musa Qureshi <musaqureshi2002@gmail.com>
2023-12-30 18:40:30 +0000

    Update package.json

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-  },
   base: "/Arabic-Learner-React-JS/",
   plugins: [react()],
 })
