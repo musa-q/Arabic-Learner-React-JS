4942071d416f31270dea7f4edd92c021d5e9d02e
Musa Qureshi <musaqureshi2002@gmail.com>
2024-02-02 02:30:23 +0000

    Fix lint 2

--- src/components/FlashCards.jsx
+++ src/components/FlashCards.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './FlashCards.css';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
