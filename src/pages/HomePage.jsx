758a9c924552aa7a184b8670b144751953185207
Musa Qureshi <musaqureshi2002@gmail.com>
2024-04-03 18:48:12 +0100

    Create feedback section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,14 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import './HomePage.css';
+import AppFeedback from '../components/AppFeedback';
 
 const HomePage = ({ onNavigate }) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return (
         <div className="home-page-container">
             <h1>Arabic Practice</h1>
@@ -9,15 +16,22 @@ const HomePage = ({ onNavigate }) => {
             <br />
             <div className="d-grid gap-5">
                 <Button variant="outline-primary" size="lg" onClick={() => onNavigate('wordsflashcard')}>
-                    Go to Words Flashcards
+                    Words Flashcards
                 </Button>
                 <Button variant="outline-primary" size="lg" onClick={() => onNavigate('wordspractice')}>
-                    Go to Words Practice
+                    Words Practice
                 </Button>
                 <Button variant="outline-primary" size="lg" onClick={() => onNavigate('verbs')}>
-                    Go to Verbs Practice
+                    Verbs Practice
+                </Button>
+                <Button variant="outline-primary" size="lg" onClick={handleShow}>
+                    Feedback
+                </Button>
+                <Button variant="outline-secondary" size="lg" disabled>
+                    Add vocab feature coming soon...
                 </Button>
             </div>
+            <AppFeedback data-bs-theme="dark" show={show} handleClose={handleClose} />
         </div>
     );
 };
